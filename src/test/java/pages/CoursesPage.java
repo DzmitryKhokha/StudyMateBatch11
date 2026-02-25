bc9d49687afc597a4b7e28e24003e79139fd7e34
Nargiza Sulaimankulova <nargizasulaimankulova@Mac.lan>
2026-02-24 19:23:10 -0600

    practice

--- src/test/java/pages/CoursesPage.java
+++ src/test/java/pages/CoursesPage.java
@@ -1,4 +1,31 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
 public class CoursesPage {
+    WebDriver driver = Driver.getDriver();
+
+    public CoursesPage(){
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(tagName  = "button")
+    public WebElement createCourseBtn;
+
+    @FindBy(xpath = "//input[@type='file']")
+    public WebElement uploadThePhoto;
+
+    @FindBy(name ="courseName")
+    public WebElement courseNameInput;
+
+    @FindBy(xpath = "//input[@placeholder='dd.mm.y']")
+    public WebElement dateInput;
+
+    @FindBy (xpath = "//button[@type='submit']")
+    public WebElement createBtn;
+
 }
