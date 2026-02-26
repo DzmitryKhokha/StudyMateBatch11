13beae6c038b0cbc986e266a0220a24568af9089
Angelina <angelina@Angelinas-MacBook-Air.local>
2026-02-26 16:02:15 -0600

    changes for Trash tab

--- src/test/java/pages/TrashPages.java
+++ src/test/java/pages/TrashPages.java
@@ -1,4 +1,30 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+import java.util.List;
 public class TrashPages {
+
+    WebDriver driver = Driver.getDriver();
+
+    public TrashPages() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//p[text()='Data successfully recovered']")
+    public WebElement dataSuccessfullyRecoveredText;
+
+    @FindBy(xpath = "//p[text()='Data deleted successfully']")
+    public WebElement SuccessfullyDeletedText;
+
+
+    @FindBy(xpath = ".//td[last()]//*[name()='svg'][1]")
+    public List<WebElement> recoverData;
+
+    @FindBy(xpath = ".//td[last()]//*[name()='svg'][2]")
+    public List<WebElement> deletePermanently;
 }
---
+++ src/test/java/tests/TrashTests.java
@@ -0,0 +1,45 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.TrashPages;
+import utils.BaseUI;
+import utils.ConfigurationReader;
+import utils.Driver;
+
+public class TrashTests extends BaseUI {
+
+
+
+    LoginPage loginPage = new LoginPage();
+    MainPage mainPage = new MainPage();
+    TrashPages trashPage = new TrashPages();
+
+    @BeforeEach
+    void loginTeachersPage() throws InterruptedException {
+        loginPage.loginWithCorrectCredentials(ConfigurationReader.getProperty("username"),
+                ConfigurationReader.getProperty("password"));
+
+        waitAndClick(mainPage.trash);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Driver.closeDriver();
+    }
+
+    @Test
+    void openTrashTab_shouldOpenTrashUrl() {
+        Assertions.assertTrue(
+                Driver.getDriver().getCurrentUrl().contains("deleted-items"),
+                "URL should contain deleted-items"
+        );
+    }
+
+
+
+    }
